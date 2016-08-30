89441b551b07b544504814df5a6aa123a803154d
“Javier” <sneiderjavier@hotmail.com>
2016-08-30 11:37:56 -0500

    Clase persona terminada

---
+++ src/clases/Persona.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author jramirez25
+ */
+public class Persona {
+
+    private long identificacion;
+    private String primer_nombre;
+    private String primer_apellido;
+
+    public Persona(long identificacion, String primer_nombre, String primer_apellido) {
+        this.identificacion = identificacion;
+        this.primer_nombre = primer_nombre;
+        this.primer_apellido = primer_apellido;
+
+    }
+
+    public long getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(long identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getPrimer_nombre() {
+        return primer_nombre;
+    }
+
+    public void setPrimer_nombre(String primer_nombre) {
+        this.primer_nombre = primer_nombre;
+    }
+
+    public String getPrimer_apellido() {
+        return primer_apellido;
+    }
+
+    public void setPrimer_apellido(String primer_apellido) {
+        this.primer_apellido = primer_apellido;
+    }
+
+}
